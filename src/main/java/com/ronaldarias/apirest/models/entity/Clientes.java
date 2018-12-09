b43178adeb5f4a85a6a230a0f975a2e6d082e1f8
JuanPaLA <juanlopezalurralde@gmail.com>
2018-12-09 19:57:32 -0300

    Introducción de Prestamos como clase interna

--- src/main/java/com/ronaldarias/apirest/models/entity/Clientes.java
+++ src/main/java/com/ronaldarias/apirest/models/entity/Clientes.java
@@ -136,3 +136,158 @@ public class Clientes {
 	}
 
 }
+
+	@Entity
+	@Table(name="prestamos")
+	class Prestamos {
+		
+		@Id
+		@GeneratedValue(strategy=GenerationType.IDENTITY)
+		@Column(name="id")
+		private Integer id;
+		
+		@Column(name="idcliente")
+		private Integer idcliente;
+		
+		@Column(name="idpagos")
+		private Integer idpagos;
+		
+		@Column(name="monto")
+		private Integer monto;
+		
+		@Column(name="tasa")
+		private float tasa;
+		
+		@Column(name="montosaldado")
+		private int montosaldado;
+		
+		@Column(name="cantidadpagos")
+		private int cantidadpagos;
+		
+		@Column(name="diaspagados")
+		private int diaspagados;
+		
+		@Column(name="diasnopagados")
+		private int diasnopagados;
+		
+		@Column(name="fechainicio")
+		private String fechainicio; // YYYY-MM-DD
+		
+		@Column(name="estado")
+		private boolean estado;
+		
+		@Column(name="pagos")
+		private int pagos;
+		
+		
+		public Prestamos() {
+			
+		}
+		
+
+		public Integer getId() {
+			return id;
+		}
+
+		public void setId(Integer id) {
+			this.id = id;
+		}
+
+		public Integer getIdcliente() {
+			return idcliente;
+		}
+
+		public void setIdcliente(Integer idcliente) {
+			this.idcliente = idcliente;
+		}
+
+		public Integer getIdpagos() {
+			return idpagos;
+		}
+
+		public void setIdpagos(Integer idpagos) {
+			this.idpagos = idpagos;
+		}
+
+		public Integer getMonto() {
+			return monto;
+		}
+
+		public void setMonto(Integer monto) {
+			this.monto = monto;
+		}
+
+		public float getTasa() {
+			return tasa;
+		}
+
+		public void setTasa(float tasa) {
+			this.tasa = tasa;
+		}
+
+		public int getMontosaldado() {
+			return montosaldado;
+		}
+
+		public void setMontosaldado(int montosaldado) {
+			this.montosaldado = montosaldado;
+		}
+
+		public int getCantidadpagos() {
+			return cantidadpagos;
+		}
+
+		public void setCantidadpagos(int cantidadpagos) {
+			this.cantidadpagos = cantidadpagos;
+		}
+
+		public int getDiaspagados() {
+			return diaspagados;
+		}
+
+		public void setDiaspagados(int diaspagados) {
+			this.diaspagados = diaspagados;
+		}
+
+		public int getDiasnopagados() {
+			return diasnopagados;
+		}
+
+		public void setDiasnopagados(int diasnopagados) {
+			this.diasnopagados = diasnopagados;
+		}
+
+		public String getFechainicio() {
+			return fechainicio;
+		}
+
+		public void setFechainicio(String fechainicio) {
+			this.fechainicio = fechainicio;
+		}
+
+		public boolean isEstado() {
+			return estado;
+		}
+
+		public void setEstado(boolean estado) {
+			this.estado = estado;
+		}
+
+		public int getPagos() {
+			return pagos;
+		}
+
+		public void setPagos(int pagos) {
+			this.pagos = pagos;
+		}
+
+		@Override
+		public String toString() {
+			return "Prestamos [id=" + id + ", idcliente=" + idcliente + ", idpagos=" + idpagos + ", monto=" + monto
+					+ ", tasa=" + tasa + ", montosaldado=" + montosaldado + ", cantidadpagos=" + cantidadpagos
+					+ ", diaspagados=" + diaspagados + ", diasnopagados=" + diasnopagados + ", fechainicio="
+					+ fechainicio + ", estado=" + estado + ", pagos=" + pagos + "]";
+		}
+		
+		
+}
