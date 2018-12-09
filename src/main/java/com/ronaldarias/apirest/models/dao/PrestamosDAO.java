af670f683397b5f18e3a49906bf9a9c268ac5f14
juanp <juanp@192.168.0.96>
2018-12-09 19:36:47 -0300

    Creación de segundo master sin clases prestamos

--- src/main/java/com/ronaldarias/apirest/models/dao/PrestamosDAO.java
+++
@@ -1,9 +0,0 @@
-package com.ronaldarias.apirest.models.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.ronaldarias.apirest.models.entity.Prestamos;
-
-public interface PrestamosDAO extends CrudRepository<Prestamos, Integer> {
-
-}
